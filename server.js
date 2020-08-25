fc193b156213eb33e18edfc849cf9b28b127b1b8
ahhzaky <siahmadzaky@gmail.com>
2020-08-25 17:41:28 +0700

    Done communication

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -10,25 +11,27 @@ const io = socketio(server);
 //Set static forlder
 app.use(express.static(path.join(__dirname, 'public')));
 
+const botName = 'ChatCord Bot';
+
 // run when client connects
 io.on('connection', socket => {
     //welocme current user
-    socket.emit('message', 'Welcome to ChatCord!');
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
 
     //broadcast when user connects
-    socket.broadcast.emit('message', 'A user has joined the chat');
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
 
     //runs when client disconnect
     socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
     });
 
     //Listen for chatMessage
     socket.on('chatMessage', msg => {
-        io.emit('message', msg);
+        io.emit('message', formatMessage('USER ', msg));
     })
 });
 
 const PORT = 3000 || process.env.PORT;
 
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
